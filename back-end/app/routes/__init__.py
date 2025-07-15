19f76ec46d6ec97ec1fa08f226a5f2f1d330c64c
monody <131kirito6518@gmail.com>
2025-07-15 19:19:26 +0800

    renew

--- back-end/app/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .audio_routes import bp as audio_bp
-from .deepseek import bp as chat_bp
-
-__all__ = ['audio_bp','chat_bp']
--- back-end/shixiaoyu_backend-main/app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .audio_routes import bp as audio_bp
-from .feihualing_routes import bp as feihualing_bp
-from .ninegrid_routes import bp as ninegrid_bp
-from .recitation_routes import bp as recitation_bp
-from .deepseek import bp as chat_bp
-
-__all__ = ['audio_bp', 'feihualing_bp', 'ninegrid_bp', 'recitation_bp','chat_bp']
